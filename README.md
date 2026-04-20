483cabd34138308a95ed3ecd221b32961d409808
Dyablouz <fernando123lawrence@gmail.com>
2026-04-20 15:14:50 +0700

    (4) Simulation of slow request.

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ handle_connection adalah sebuah fungsi yang mengambil data stream dan ditandai d
 Mengecek request yang dikirim dari user, jika pathnya sesuai, maka refer user ke halaman hello.html dengan status "OK". Jika tidak sesuai, maka tampilkan file 404.html dan status "NOT FOUND".
 
 ![Commit 3 screen capture](/assets/images/commit3.png)
+
+## Commit 4
+Ketika kita membuka request user berupa 127.0.0.1/sleep, terdapat jeda 10 detik sampai halaman user ter-load dengan baik dikarenakan kita menambahkan "thread::sleep(Duration::from_secs(10));". Hal ini berguna untuk mengatur agar request yang diterima server ketika banyak user mengaksesnya sekaligus untuk tidak kewalahan menghandlenya karena terdapat antrian.
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -23,14 +25,20 @@ fn handle_connection(mut stream: TcpStream){
         
     let (status_line,filename) = if http_request[0] == "GET / HTTP/1.1"{
         ("HTTP/1.1 200 OK", "hello.html")
-    } else{
+    }
+    else if http_request[0] == "GET /sleep HTTP/1.1"{
+        thread::sleep(Duration::from_secs(10));
+        ("HTTP/1.1 200 OK", "hello.html")
+    }
+    else{
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
+
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = 
-        format!("{status_line}\r\nContent-Length; {length}\r\n\r\n{contents}");
+        format!("{status_line}\r\nContent-length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
